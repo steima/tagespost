aa46be3c023672e1220f2d0e6af0feabf1fd962a
steima <matthias@steinbauer.org>
2021-10-11 11:55:32 +0200

    fix: Tests for filename

--- src/test/java/at/demanda/lib/tagespost/TagesPostClientTest.java
+++ src/test/java/at/demanda/lib/tagespost/TagesPostClientTest.java
@@ -13,34 +13,34 @@ import at.demanda.lib.tagespost.enums.DuplexPrint;
 import at.demanda.lib.tagespost.enums.EnvelopeLogo;
 
 public class TagesPostClientTest {
-	
+
 	static Logger logger = LoggerFactory.getLogger(TagesPostClientTest.class);
-	
+
 	@Test
 	public void testTagesPostFile() {
 		String filename = "mahnung.pdf";
 		TagesPostJobSettings f = new TagesPostJobSettings(Approval.ManualApproval, filename);
-		String expected = "000000000000000-mahnung.pdf";
+		String expected = "in/bulk_tp/000000000000000-mahnung.pdf";
 		String actual = f.generateFilename();
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testTagesPostFileWithUmlauts() {
 		String filename = "mahnung-Österreicher.pdf";
 		TagesPostJobSettings f = new TagesPostJobSettings(Approval.ManualApproval, filename);
-		String expected = "000000000000000-mahnung__sterreicher.pdf";
+		String expected = "in/bulk_tp/000000000000000-mahnung__sterreicher.pdf";
 		String actual = f.generateFilename();
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testTagesPostFileWithUmlautsColorDuplexNationalRegisteredNoLogo() {
 		String filename = "mahnung-Österreicher.pdf";
 		TagesPostJobSettings f = new TagesPostJobSettings(Approval.ManualApproval, ColorPrint.Color, DuplexPrint.Duplex, DeliveryType.NationalRegistered, EnvelopeLogo.No, filename);
-		String expected = "001100100000000-mahnung__sterreicher.pdf";
+		String expected = "in/bulk_tp/001100100000000-mahnung__sterreicher.pdf";
 		String actual = f.generateFilename();
 		assertEquals(expected, actual);
 	}
-	
+
 }
